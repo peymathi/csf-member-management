0948f6934109716ed479b8d9f0fa6329ccb4b89b
Tucker Dooley <tucker.dooley1234@gmail.com>
2019-10-30 14:43:08 -0400

    initial commit

---
+++ login.php
@@ -0,0 +1,11 @@
+ <?php  session_start(); //this must be the very first line on the php page, to register this page to use session variables
+	
+	
+
+	
+?>
+<html>
+<body>
+<p>test</p>
+</body>
+</html>
