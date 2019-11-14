c6477ff9c644281134b84fa210b46431b0cf262a
Paul Joy Jr <pjoyjr@tesla.cs.iupui.edu>
2019-11-14 16:13:31 -0500

    add blank page template

---
+++ black_page.php
@@ -0,0 +1,11 @@
+<?php
+    include 'header.php';
+?>
+<body>
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>Title Here</h1> <!--Add title to header.php as well for tab name-->
+</div>
+
+
+</body>
+</html>
