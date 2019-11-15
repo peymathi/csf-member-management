2e4a800644bc61146bc7fdaa978e08c4aa1795a5
Tucker Dooley <tucker.dooley1234@gmail.com>
2019-11-15 01:05:53 -0500

    wont work locally cause need email server

--- forgot_password.php
+++ forgot_password.php
@@ -1,20 +1,88 @@
 <?php 
 include 'header.php';
+
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+	header("location: dashboard.php");
+	exit;
+}
+
+require_once "config.php";
+
+$email = "";
+
+$email_error = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+	if(empty(trim($_POST["email"]))){
+		$email_error = "Must enter email.";
+	} else {
+		$email = trim($_POST["email"]);
+	}
+
+	if(empty($email_error)){
+		//$sql = "SELECT id, email, password FROM admins WHERE email = ?";
+
+		$sql = "UPDATE admins SET password = ? WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_password, $param_email);
+
+			$newpassword = randomPassword();//unhashed
+			$param_password = password_hash($newpassword, PASSWORD_DEFAULT);
+			$param_email = $email;
+			
+			if(mysqli_stmt_execute($stmt)){
+				
+				echo $newpassword;
+				$to = $email;
+				$subject = "Password Reset";
+                $txt = "Your new password is : $newpassword";
+                $headers = "From: test@test.com";
+                mail($to,$subject,$txt,$headers);
+				
+			} else{
+				//echo "Oops! Something went wrong. Please try again later.";
+			}
+			echo "An email was sent if there is a user with that email.";
+		}
+		
+		mysqli_stmt_close($stmt);
+	}
+	
+	mysqli_close($link);
+}
+
+
+function randomPassword() {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < 8; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
+
+
 ?>
 <body>
 
 <div class="jumbotron text-center" style="margin-bottom:0">
-  <h1>Password Reset</h1>
+  <h1>Forgot Password</h1>
 </div>
 <div class="container" style="margin-top:30px">
 	<div class="row">
 		<div class="col">
-			<form action="/action_page.php" class="needs-validation" novalidate>
+			<form action="" method="post" class="needs-validation" novalidate>
 			  <div class="form-group">
 				<label for="email">Email:</label>
 				<input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
 				<div class="valid-feedback">Valid.</div>
 				<div class="invalid-feedback">Please fill out this field.</div>
+				<span class="help-block"><?php echo $email_error; ?></span>
 			  </div>
 			  <button type="submit" class="btn btn-primary">Reset</button>
 			</form>
@@ -22,7 +90,7 @@ include 'header.php';
 	</div>
 	<div class="row mt-2">
 		<div class="col">
-			<button type="submit" class="btn btn-secondary">Back To Log In</button>
+			<button type="submit" href="login.php" class="btn btn-secondary">Back To Log In</button>
 		</div>
 	</div>
 	<script>
