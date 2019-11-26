ef61962b4cb786a9526c1679222c73787d7032e5
Corey Stockton <coreystockton4c@gmail.com>
2019-11-26 18:27:55 -0500

    added connect fnct option

--- db_connect.php
+++ db_connect.php
@@ -19,4 +19,17 @@ catch(PDOException $e)
     {
     echo $e->getMessage();
     }
+
+// Returns the connection object to the database.
+function db_connect($hostname = 'localhost', $username = 'root', $password = '')
+{
+  try
+  {
+    return new PDO("mysql:host=$hostname;dbname=csfi_db", $username, $password);
+  }
+  catch(PDOException $e)
+  {
+    echo $e->getMessage();
+  }
+}
 ?>
