9b0a7817d3572e55ea513dada9986a3f107393ac
Paul Joy Jr <pjoyjr@tesla.cs.iupui.edu>
2019-12-01 17:27:23 -0500

    added reports dashboard

---
+++ headerReports.php
@@ -0,0 +1,25 @@
+<?php
+echo	
+	'<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="dashboard.php">Main Menu</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Prayer Requests</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Contact Emails/Texts</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Life Groups</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Not in Life Groups</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Home Addresses</a>
+			</li>
+		</ul>
+	</nav>'
+?>
