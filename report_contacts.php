07f90137cb224431696ddcdd76ca9da668aec2ad
Paul Joy Jr <pjoyjr@tesla.cs.iupui.edu>
2019-12-01 17:54:33 -0500

    working contacts report

--- report_contacts.php
+++ report_contacts.php
@@ -1,7 +1,7 @@
 <?php
     include 'header.php';
     session_verify();
-
+	require_once 'db_connect.php';
 ?>
 <body>
 
@@ -16,7 +16,10 @@
   <div class="row">
 
     <div class="col">
-
+		<!-- Contact Emails: Select FirstName, LastName, Email From Members Where OptEmail = True
+			}
+		-->
+		<h3>Opted in to Email</h3>
 		<table class="table">
 			<thead class="thead-dark">
 			  <tr>
@@ -26,24 +29,39 @@
 			  </tr>
 			</thead>
 			<tbody>
-			  <tr>
-				<td>John</td>
-				<td>Doe</td>
-				<td>john@example.com</td>
-			  </tr>
-			  <tr>
-				<td>Mary</td>
-				<td>Moe</td>
-				<td>mary@example.com</td>
-			  </tr>
-			  <tr>
-				<td>July</td>
-				<td>Dooley</td>
-				<td>july@example.com</td>
-			  </tr>
+			<?php
+				$emailStmt = $con->prepare("Select FirstName, LastName, EmailAddress From Members Where OptEmail = 1");
+				$emailStmt->execute();
+				while($emailRow = $emailStmt->fetch(PDO::FETCH_ASSOC)) {
+					echo '<tr>';
+					echo '<td>'.$emailRow['FirstName'].'</td><td>'.$emailRow['LastName'].'</td><td>'.$emailRow['EmailAddress'].'</td>';
+					echo '</tr>';
+				}
+			?>
 			</tbody>
-		  </table>
-
+		</table>
+		<!-- Contact Texts: Select FirstName, LastName, PhoneNumber From Members Where OptTexts = True -->
+		<h3>Opted in to Text</h3>
+		<table class="table">
+			<thead class="thead-dark">
+			  <tr>
+				<th>Firstname</th>
+				<th>Lastname</th>
+				<th>Phone Number</th>
+			  </tr>
+			</thead>
+			<tbody>
+			<?php
+				$emailStmt = $con->prepare("Select FirstName, LastName, PhoneNumber From Members Where OptText = 1");
+				$emailStmt->execute();
+				while($emailRow = $emailStmt->fetch(PDO::FETCH_ASSOC)) {
+					echo '<tr>';
+					echo '<td>'.$emailRow['FirstName'].'</td><td>'.$emailRow['LastName'].'</td><td>'.$emailRow['PhoneNumber'].'</td>';
+					echo '</tr>';
+				}
+			?>
+			</tbody>
+		</table>
 
      </div>
   </div>
