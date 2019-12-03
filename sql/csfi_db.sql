debf044de2fda98461d70f4432d4966227db704f
Stockton, Corey Taylor <ctstockt@iupui.edu>
2019-12-02 19:50:45 -0500

    finished db
    ...finally...

--- sql/csfi_db.sql
+++ sql/csfi_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2019 at 08:37 PM
+-- Generation Time: Dec 03, 2019 at 01:39 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -25,28 +25,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `groups`
 --
 
-DROP TABLE IF EXISTS `groups`;
-CREATE TABLE `groups` (
-  `GroupID` int(11) NOT NULL,
-  `GroupName` varchar(256) NOT NULL
+CREATE TABLE IF NOT EXISTS `groups` (
+  `GroupID` int(11) NOT NULL AUTO_INCREMENT,
+  `GroupName` varchar(256) NOT NULL,
+  PRIMARY KEY (`GroupID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `groups`
---
-
-INSERT INTO `groups` (`GroupID`, `GroupName`) VALUES
-(1, 'Freshman'),
-(2, 'Sophomore'),
-(3, 'Junior'),
-(4, 'Senior'),
-(5, 'Graduate'),
-(6, 'Alumni'),
-(7, 'Staff'),
-(8, 'Other');
 
 -- --------------------------------------------------------
 
@@ -54,10 +55,13 @@ INSERT INTO `groups` (`GroupID`, `GroupName`) VALUES
 -- Table structure for table `life_groups`
 --
 
-DROP TABLE IF EXISTS `life_groups`;
-CREATE TABLE `life_groups` (
-  `LifeGroupID` int(11) NOT NULL,
-  `LifeGroupName` varchar(256) NOT NULL
+CREATE TABLE IF NOT EXISTS `life_groups` (
+  `LifeGroupID` int(11) NOT NULL AUTO_INCREMENT,
+  `LifeGroupName` varchar(256) NOT NULL,
+  `LifeGroupDay` varchar(9) NOT NULL,
+  `LifeGroupTime` time NOT NULL,
+  `LifeGroupLocation` varchar(256) NOT NULL,
+  PRIMARY KEY (`LifeGroupID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -66,19 +70,22 @@ CREATE TABLE `life_groups` (
 -- Table structure for table `members`
 --
 
-DROP TABLE IF EXISTS `members`;
-CREATE TABLE `members` (
-  `MemberID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `members` (
+  `MemberID` int(11) NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(256) NOT NULL,
   `LastName` varchar(256) NOT NULL,
   `EmailAddress` varchar(256) DEFAULT NULL,
   `HomeAddress` varchar(512) DEFAULT NULL,
   `PhoneNumber` varchar(64) NOT NULL,
+  `Major` varchar(255) DEFAULT 'NULL',
   `PhotoPath` varchar(256) DEFAULT NULL,
   `PrayerRequest` varchar(512) DEFAULT NULL,
   `OptEmail` tinyint(1) NOT NULL,
   `OptText` tinyint(1) NOT NULL,
-  `GroupID` int(11) NOT NULL
+  `GroupID` int(11) NOT NULL,
+  PRIMARY KEY (`MemberID`),
+  UNIQUE KEY `PhoneNumber` (`PhoneNumber`),
+  KEY `GroupID` (`GroupID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -87,22 +94,28 @@ CREATE TABLE `members` (
 -- Table structure for table `member_life_group_junction`
 --
 
-DROP TABLE IF EXISTS `member_life_group_junction`;
-CREATE TABLE `member_life_group_junction` (
+CREATE TABLE IF NOT EXISTS `member_life_group_junction` (
+  `MemberLifeGroupID` int(11) NOT NULL AUTO_INCREMENT,
   `MemberID` int(11) NOT NULL,
-  `LifeGroupID` int(11) NOT NULL
+  `LifeGroupID` int(11) NOT NULL,
+  PRIMARY KEY (`MemberLifeGroupID`),
+  KEY `LifeGroupID` (`LifeGroupID`),
+  KEY `MemberID` (`MemberID`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `member_night_junction`
+-- Table structure for table `member_nights_of_worship_junction`
 --
 
-DROP TABLE IF EXISTS `member_night_junction`;
-CREATE TABLE `member_night_junction` (
+CREATE TABLE IF NOT EXISTS `member_nights_of_worship_junction` (
+  `MemberNightID` int(11) NOT NULL AUTO_INCREMENT,
   `MemberID` int(11) NOT NULL,
-  `NightID` int(11) NOT NULL
+  `NightID` int(11) NOT NULL,
+  PRIMARY KEY (`MemberNightID`),
+  KEY `MemberID` (`MemberID`),
+  KEY `NightID` (`NightID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -111,83 +124,11 @@ CREATE TABLE `member_night_junction` (
 -- Table structure for table `nights_of_worship`
 --
 
-DROP TABLE IF EXISTS `nights_of_worship`;
-CREATE TABLE `nights_of_worship` (
-  `NightID` int(11) NOT NULL,
-  `NightDate` date NOT NULL
+CREATE TABLE IF NOT EXISTS `nights_of_worship` (
+  `NightID` int(11) NOT NULL AUTO_INCREMENT,
+  `NightDate` date NOT NULL,
+  PRIMARY KEY (`NightID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `groups`
---
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`GroupID`);
-
---
--- Indexes for table `life_groups`
---
-ALTER TABLE `life_groups`
-  ADD PRIMARY KEY (`LifeGroupID`);
-
---
--- Indexes for table `members`
---
-ALTER TABLE `members`
-  ADD PRIMARY KEY (`MemberID`),
-  ADD UNIQUE KEY `PhoneNumber` (`PhoneNumber`),
-  ADD KEY `GroupID` (`GroupID`);
-
---
--- Indexes for table `member_life_group_junction`
---
-ALTER TABLE `member_life_group_junction`
-  ADD PRIMARY KEY (`MemberID`,`LifeGroupID`),
-  ADD KEY `LifeGroupID` (`LifeGroupID`);
-
---
--- Indexes for table `member_night_junction`
---
-ALTER TABLE `member_night_junction`
-  ADD PRIMARY KEY (`MemberID`,`NightID`),
-  ADD KEY `NightID` (`NightID`);
-
---
--- Indexes for table `nights_of_worship`
---
-ALTER TABLE `nights_of_worship`
-  ADD PRIMARY KEY (`NightID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `groups`
---
-ALTER TABLE `groups`
-  MODIFY `GroupID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `life_groups`
---
-ALTER TABLE `life_groups`
-  MODIFY `LifeGroupID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `members`
---
-ALTER TABLE `members`
-  MODIFY `MemberID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `nights_of_worship`
---
-ALTER TABLE `nights_of_worship`
-  MODIFY `NightID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -207,11 +148,11 @@ ALTER TABLE `member_life_group_junction`
   ADD CONSTRAINT `member_life_group_junction_ibfk_2` FOREIGN KEY (`LifeGroupID`) REFERENCES `life_groups` (`LifeGroupID`);
 
 --
--- Constraints for table `member_night_junction`
+-- Constraints for table `member_nights_of_worship_junction`
 --
-ALTER TABLE `member_night_junction`
-  ADD CONSTRAINT `member_night_junction_ibfk_1` FOREIGN KEY (`MemberID`) REFERENCES `members` (`MemberID`),
-  ADD CONSTRAINT `member_night_junction_ibfk_2` FOREIGN KEY (`NightID`) REFERENCES `nights_of_worship` (`NightID`);
+ALTER TABLE `member_nights_of_worship_junction`
+  ADD CONSTRAINT `member_nights_of_worship_junction_ibfk_1` FOREIGN KEY (`MemberID`) REFERENCES `members` (`MemberID`),
+  ADD CONSTRAINT `member_nights_of_worship_junction_ibfk_2` FOREIGN KEY (`NightID`) REFERENCES `nights_of_worship` (`NightID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
