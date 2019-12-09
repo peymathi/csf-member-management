ddcc88bb59b91405fedef675f229f5a683c8880e
ZAIDS-PC\Zaid <zaidsaeedzaid@gmail.com>
2019-12-09 00:30:33 -0500

    Adding list of members to dashboard

--- dashboard.php
+++ dashboard.php
@@ -37,6 +37,78 @@
   <div class="row">
 
     <div class="col">
+      <table id="example" class="display nowrap" style="width:100%">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Prayer</th>
+            <th>OptEmail</th>
+            <th>OptText</th>
+            <th>Group ID</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+              //loop through members
+              $MembersStmt = $con->query("SELECT * FROM members");
+              while($MembersRow = $MembersStmt->fetch(PDO::FETCH_ASSOC)) {
+                echo "<tr>";
+                
+                echo "<td>" . $MembersRow['FirstName'] . "</td>";
+                echo "<td>" . $MembersRow['LastName'] . "</td>";
+                echo "<td>" . $MembersRow['EmailAddress'] . "</td>";
+                echo "<td>" . $MembersRow['HomeAddress'] . "</td>";
+                echo "<td>" . $MembersRow['PhoneNumber'] . "</td>";
+                echo "<td>" . $MembersRow['PrayerRequest'] . "</td>";
+                
+                if($MembersRow['OptEmail'] == "0"){
+                  echo "<td>No</td>";
+                }else{
+                  echo "<td>Yes</td>";
+                }
+
+                if($MembersRow['OptText'] == "0"){
+                  echo "<td>No</td>";
+                }else{
+                  echo "<td>Yes</td>";
+                }
+                
+                echo "<td>" ; 
+                $GroupStmt = $con->prepare("SELECT GroupName FROM groups WHERE GroupID = :GroupID");
+                $GroupStmt->execute(array('GroupID' => $MembersRow['GroupID']));
+                while($Group1Row = $GroupStmt->fetch(PDO::FETCH_ASSOC)) {
+                  echo $Group1Row['GroupName'];
+                }
+                echo "</td>";
+                
+
+                echo '<td>
+                <form action="" method="post">
+                <input type="hidden" id="editmember" name="editmember" value="'. $MembersRow['MemberID'] . '">
+                <button type="submit" class="btn btn-secondary">Edit</button>
+
+                </form>
+                </td>';
+
+                echo '<td>
+                <form action="" method="post">
+                <input type="hidden" id="deletemember" name="deletemember" value="'. $MembersRow['MemberID'] . '">
+                <button type="submit" class="btn btn-danger">Delete</button>
+
+                </form>
+                </td>';
+                
+                echo "</tr>";
+              }
+          ?>
+        </tbody>
+      </table>
      </div>
   </div>
 </div>
